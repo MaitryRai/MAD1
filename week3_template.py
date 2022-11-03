a38e097e497f382d001b28866b67704cbb51919d
maitry <96180798+MaitryRai@users.noreply.github.com>
2022-11-04 02:54:39 +0530

    Templates.
    Here we rewrite our first python program using templates. These are just simple string templates, they are good for inline templating. However, for large chunks of code, we need a templating engine, which we will introduce next.

--- week3_template.py
+++ week3_template.py
@@ -6,9 +6,10 @@ def main():
 if __name__=="__main__":
     main()
     
-
+    
+    
 program2:
-
+    
 TEMPLATE="""This is {p:+} and this is {n:+}"""
 def main():
   print(TEMPLATE.format(p=5,n=-7))
