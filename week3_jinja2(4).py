98c514ee560a8de4c9c9d4b6c9ad8ea90f5d4ce1
maitry <96180798+MaitryRai@users.noreply.github.com>
2022-11-07 12:47:49 +0530

    Writing HTML output to a file
    Here, instead of printing the output of the rendered template, we write it to a file.

---
+++ week3_jinja2(4).py
@@ -0,0 +1,50 @@
+"""Now, instead of printing the generated template output, we write it in a file"""
+
+from jinja2 import Template
+
+forbes_data=[{"Name":"Raghuram G Rajan","Domain":"Economic Policy","Country":"India"},
+             {"Name":"Raj Chetty","Domain":"Data & Policy","Country":"USA"},
+             {"Name":"Rishi Sunak","Domain":"Politics","Country":"Britain"},
+             {"Name":"Chinmay Tumbe","Domain":"Social Policy","Country":"India"},
+            {"Name":"Sundar Pichai","Domain":"Technology","Country":"USA"}]
+TEMPLATE=""" 
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8"/>
+ <title>Forbes</title>
+</head>
+<body>
+<h1>People Of The Year</h1>
+<table>
+<thead>
+ <tr>
+    <th>Name</th>
+     <th>Domain</th>
+     <th>Country</th>
+  </tr>
+ </thead>
+ <tbody>
+   {% for person in forbes_data %}
+    <tr>
+    <td>{{person["Name"] }}</td>
+    <td>{{person["Domain"] }}</td>
+    <td>{{person["Country"]}}</td>
+    </tr>
+  {% endfor %}
+  </tbody>
+</table>
+</body>
+</html>
+"""
+
+def main():
+  template=Template(TEMPLATE)
+  content=template.render(forbes_data=forbes_data)
+  
+  my_file=open("forbes.html","w")
+  my_file.write(content)
+  my_file.close()
+
+if __name__=="__main__":
+  main()
